a6c5fa377f9014c94c3a638a710bf3e0e80ef11e
ranny440 <tebeckransom@gmail.com>
2025-03-01 22:24:27 +0100

    settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Bot',
   botName: "Knight Bot",
   botOwner: 'Knight Bot', // Your name
-  ownerNumber: '237653131536', //Your number
+  ownerNumber: '+237653131536', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
